432e186f1ebf9aab88ab6ceed4bd01f5ced81603
Bhoomikarwani <bhoomikarwani@gmail.com>
2025-10-28 22:24:04 +0530

    prime no.s from 1 to n

---
+++ functions/prime.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+bool isPrime(int num){
+  if (num==1)
+     return false;
+  for(int i=2;i<num ;i++){
+    if(num%i==0){
+      return false;
+    }
+   }
+   return true;
+}
+
+int main(){
+  int n=10;
+  for(int i=1; i<=n ;i++){
+    if(isPrime(i)){
+      cout<< i <<endl;
+    }
+  }
+  return 0;
+}
