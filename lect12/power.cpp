b90baab2fbb1b2a8cd217b354e697f5f31a9e352
Bhoomikarwani <bhoomikarwani@gmail.com>
2025-11-25 22:34:30 +0530

    power & stock buy and sell

---
+++ lect12/power.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+//pow(x^n)
+double myPow(double x, int n){
+    double ans=1;
+    int binaryForm=n;
+    if(n==0) return 1.0;
+    if(x==0) return 0.0;
+    if(x==1) return 1.0;
+    if(x==-1 && n%2==0) return 1.0;
+    if(x==-1 && n%2!=0) return -1.0;
+
+    //for negative power
+    if(n<0){
+        x=1/x;
+        binaryForm= -binaryForm;
+    }
+
+    while(binaryForm >0){
+         if(binaryForm%2==1){
+            ans*=x;
+         }
+         x*=x;
+         binaryForm /=2;
+    }
+
+    return ans;
+}
+
+int main(){
+    double x=3.0;
+    int n=5; //in binary form
+    cout<< myPow(x,n);
+}
---
+++ lect12/stock.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int buy_and_sell(vector<int> &prices){
+   
+   int mp=0;    //max. profit
+   int bestBuy=prices[0];
+   for(int i=1 ; i<prices.size() ; i++){
+         if(prices[i]>bestBuy){
+            mp=max(mp , prices[i]-bestBuy);
+         }
+         bestBuy=min(bestBuy,prices[i]);
+   }
+
+   return mp;
+
+}
+int main(){
+    vector<int> prices={7,1,5,3,6,4};
+    cout<< buy_and_sell(prices)<<endl;
+}
