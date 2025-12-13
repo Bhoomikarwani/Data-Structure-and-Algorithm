b7a76f9f0bb111744f3a3e734ba67a3811e853c2
Bhoomikarwani <bhoomikarwani@gmail.com>
2025-12-13 18:24:51 +0530

    painters problem

---
+++ lect21/painters.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+bool isPossible(vector<int> &arr ,int n ,int m , int maxAllowedTime){
+    int painters = 1, time = 0;
+    for (int i = 0; i < n; i++){
+        if (time + arr[i] <= maxAllowedTime)
+        {
+            time += arr[i];
+        }
+        else
+        {
+            painters++;
+            time = arr[i];
+        }
+    }   
+    return painters <= m;
+}
+
+int  minTimeToPaint(vector<int> &arr ,int n ,int m){
+    int st, end, ans=-1;
+    int sum = 0, maxVal = INT16_MIN;
+    for (int i = 0; i < n; i++)
+    {
+        sum += arr[i];
+        maxVal = max(maxVal, arr[i]);
+    }
+    st = maxVal;
+    end = sum;
+    while (st <= end)
+    {
+        int mid = st + (end + st) / 2;
+        if (isPossible(arr, n, m, mid))  //left
+        {
+            ans = mid;
+            end = mid - 1;
+        }
+        else         //right
+        {
+            st = mid + 1;
+        }
+    }
+    return ans;
+}
+
+int main(){
+    vector<int> arr={40,30,10,20};
+    int n=4, m=2;    //no. of borads    //no. of painters
+    cout<< minTimeToPaint(arr,n,m)<<endl;
+    return 0;
+}
