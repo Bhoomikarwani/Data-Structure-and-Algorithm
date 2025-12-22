fca5e6e934fa44cec35a3abc5883a2c0dc03e06f
Bhoomikarwani <bhoomikarwani@gmail.com>
2025-12-22 23:47:58 +0530

    mountain

--- lect18/mountain.cpp
+++ lect18/mountain.cpp
@@ -3,11 +3,11 @@
 using namespace std;
 
 int peakIndexInMountainArray(vector<int>& A){
-   int st=1,end=A.size()-2;
+   int st=0,end=A.size()-1;
    while (st<=end)
    {
     int mid=st+ (end-st)/2;
-    if(A[mid-1]<A[mid] && A[mid]<A[mid+1]){
+    if(A[mid-1]<A[mid] && A[mid]>A[mid+1]){
         return mid;
     }
     else if(A[mid-1]<A[mid] ){  //in increasing order
@@ -24,3 +24,17 @@ int main(){
   vector<int> A={0,3,8,9,5,2};
   cout<<peakIndexInMountainArray(A);
 }
+
+
+//or
+//low=0, high=size-1
+//while(low<high){
+//   mid=low+high/2
+//   if(arr[mid]<arr[mid+1]){
+//      low=mid+1; 
+//    }
+//   else{
+//      high=mid;  
+//   }
+//}
+//return low;
