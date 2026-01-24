f33d8e8ce844a412897907465b144d8d7f915cef
Bhoomi Karwani <bhoomikarwani@gmail.com>
2026-01-24 20:51:20 +0530

    list container

--- lect26/stlListContainer.cpp
+++ lect26/stlListContainer.cpp
@@ -2,6 +2,26 @@
 #include<vector>
 #include<list>
 using namespace std;
+
+//same as vector but
+
+
+//list is like a doubly linked list , we can push elements in list from ending as well as from starting
+
 int main(){
-    
+  list<int> l={1,2,3};   
+  l.emplace_back(1);
+  l.push_back(2);
+  l.push_front(3);
+  for(int val: l){
+    cout<< val;
+  }
+  cout<< endl;
+  l.pop_back();
+  l.pop_front();
+
+  for(int val: l){
+    cout<< val;
+  }
+
 }
