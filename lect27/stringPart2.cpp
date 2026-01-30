38f1d093680747a252041122a4eed8f24f64ea78
Bhoomi Karwani <bhoomikarwani@gmail.com>
2026-01-30 23:30:44 +0530

    part 2 of strings is completed

--- lect27/stringPart2.cpp
+++ lect27/stringPart2.cpp
@@ -4,34 +4,49 @@
 using namespace std;
 
 //check whether the given phrase is palindrome or not (ingnore non-alphanumeric characters and convert all uppercase char to lowercase)
-bool isAlphaNum(char ch){
-   if((ch >= '0' && ch<= '9')  || (tolower(ch) >= 'a' && tolower(ch) >= 'z')){
-    return true;
-   }
-   return false;
-}
+// bool isAlphaNum(char ch){
+//    if((ch >= '0' && ch<= '9')  || (tolower(ch) >= 'a' && tolower(ch) <= 'z')){
+//     return true;
+//    }
+//    return false;
+// }
 
-bool isPalindrome(string s){
-   int st=0 , end =s.length()-1;
-   while(st<end){
-      if(! isAlphaNum(s[st])){
-        st++;
-        continue;
-      }
-      if(! isAlphaNum(s[end])){
-        end--;
-        continue;
-      }
-      if(tolower(s[st]) != tolower(s[end]) ){
-        return false;
-      }
-      st++;
-      end--;
-   }
-   return true;
+// bool isPalindrome(string s){
+//    int st=0 , end =s.length()-1;
+//    while(st<end){
+//       if(! isAlphaNum(s[st])){
+//         st++;
+//         continue;
+//       }
+//       if(! isAlphaNum(s[end])){
+//         end--;
+//         continue;
+//       }
+//       if(tolower(s[st]) != tolower(s[end]) ){
+//         return false;
+//       }
+//       st++;
+//       end--;
+//    }
+//    return true;
+// }
+// int main(){
+//     string s="A man, a plan, a canal: Panama";
+//     cout << isPalindrome(s);
+//    return 0;
+//}
+
+
+//remove all occurence of part from 's' start from left side
+string removeOccurence(string s, string part){
+  while(s.length() > 0 &&  s.find(part) < s.length()){   //s.find(part)-returns index value
+    s.erase(s.find(part) , part.length());   //s.erase(start, jitna part delete hona hai uski length)
+  }
+  return s;
 }
 int main(){
-    string s="A man, a plan, a canal: Panama";
-    cout << isPalindrome(s);
-    return 0;
+  string s="daabcbaabcbc";
+  string part="abc";
+  cout << removeOccurence(s, part);
+  return 0;
 }
