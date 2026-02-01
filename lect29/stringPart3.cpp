2739a477174196ee52662b583c701c9509a957a2
Bhoomi Karwani <bhoomikarwani@gmail.com>
2026-02-01 23:25:39 +0530

    part 3 of strings

--- lect29/stringPart3.cpp
+++ lect29/stringPart3.cpp
@@ -4,17 +4,19 @@ using namespace std;
 
     bool isFreqSame(int freq1[], int freq2[]){
        for(int i=0; i<26; i++){ 
-        if(freq1[i] != freq2[i]){
+          if(freq1[i] != freq2[i]){
             return false;
-        }
+          }
        } 
        return true;
     }
     bool checkExistenceofPermutation(string s1, string s2){
         int freq[26]= {0};
         for(int i=0 ; i <s1.length() ; i++){
-            freq[s1[i]- 'a']++;
+            int idx=s1[i]- 'a';
+            freq[idx]++;
         }
+
         int windSize=s1.length();
 
         for(int i=0 ; i< s2.length() ; i++){
