7efa695aa2314bea54b4d7edd83b0cc4817a9eea
Bhoomi Karwani <bhoomikarwani@gmail.com>
2026-02-08 15:11:29 +0530

    prime numbers in a given range

---
+++ lect32/math.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+//to one number
+string isPrime(int n){
+    for(int i=2; i*i <= n ; i++){
+        if(n % i==0){
+            return "not prime";
+        }
+    }
+    return "prime";
+}
+
+//to check for range of numbers(brute force approach)
+// string isPrimeRange(int N)
+// {
+//     for (int n = 2; n <= N; n++)
+//     {
+//         bool isPrime = true;
+
+//         for (int i = 2; i * i <= n; i++)
+//         {
+//             if (n % i == 0)
+//             {
+//                 isPrime = false;
+//                 break;
+//             }
+//         }
+//         if (isPrime)
+//         {
+//             cout << n << endl;
+//         }
+//     }
+// }
+
+//to check for range of numbers(optimize approach)
+//sieve of Eratosthenes
+int PrimeInRange(int N){
+
+     vector<bool> prime(N+1,true);
+     int count =0;
+
+     for(int i=2 ; i<N ; i++){
+         if(prime[i]==true){
+            count++;
+            for(int j=i*2 ; j<N ; j=j+i){
+                prime[j]=false;
+            }
+         } 
+     }
+     return count;
+}
+
+int main(){
+    // int n=47;
+    // cout<< isPrime(n)<< endl;
+
+    //cout << isPrimeRange(50) <<endl;
+
+    cout << PrimeInRange(50) <<endl ; 
+    return 0;
+}
