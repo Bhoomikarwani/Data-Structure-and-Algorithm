184e5ee1901b6872a4db536a283b70718f177942
Bhoomi Karwani <bhoomikarwani@gmail.com>
2026-03-12 01:01:24 +0530

    2D maxsum

--- lect33/2darray.cpp
+++ lect33/2darray.cpp
@@ -24,30 +24,80 @@ using namespace std;
     
 //}    
     
-pair<int,int> LinearSearch(int key , int rows , int cols , int matrix[][3]){
-        for(int i=0 ; i < rows ; i++){
-            for(int j=0 ; j < cols ; j++){
-               if(matrix[i][j]== key){
-                   return {i,j};
-               }
-            }
-        }
-        return {-1,-1};
-    }
+// pair<int,int> LinearSearch(int key , int rows , int cols , int matrix[][3]){
+//         for(int i=0 ; i < rows ; i++){
+//             for(int j=0 ; j < cols ; j++){
+//                if(matrix[i][j]== key){
+//                    return {i,j};
+//                }
+//             }
+//         }
+//         return {-1,-1};
+// }
+// int main(){ 
+//     int matrix[4][3] = { { 1,2,3}, {4,5,6} , {7,8,9} , { 10, 11, 12}};
+//     int rows =4;
+//     int cols =3 ;
+//     pair<int , int> result=LinearSearch(10,  rows , cols , matrix);
+//     if(result.first != -1){
+//         cout << "(" << result.first << ","<< result.second<< ")" << endl;
+//     }
+//     else{
+//         cout << "key not found";
+//     }
+//     //cout << LinearSearch(3,  rows , cols , matrix);
+//     return 0;
+// }
 
-int main(){ 
-    int matrix[4][3] = { { 1,2,3}, {4,5,6} , {7,8,9} , { 10, 11, 12}};
-    int rows =4;
-    int cols =3 ;
-    pair<int , int> result=LinearSearch(10,  rows , cols , matrix);
-    if(result.first != -1){
-        cout << "(" << result.first << ","<< result.second<< ")" << endl;
+
+//maximum row sum
+//1 2 3
+//4 5 6
+//7 8 9
+int maxRowSum(int matrix[][3], int rows , int cols){
+ int maxsum=0;
+ for( int i=0 ; i< rows ; i++){
+    int sum = 0;
+    for(int j=0 ; j< cols ;j++){
+        sum = sum + matrix[i][j];
     }
-    else{
-        cout << "key not found";
-    }
-    //cout << LinearSearch(3,  rows , cols , matrix);
-    
-    return 0;
+    // if(maxsum < sum ){
+    //     maxsum = sum ;
+    // }
+    maxsum = max(sum , maxsum);
+ }
+ return maxsum;
 }
 
+int maxColSum(int matrix[][3], int rows, int cols)
+{
+    int maxsum = 0;
+    for (int i = 0; i < rows; i++)
+    {
+        int sum = 0;
+        for (int j = 0; j < cols; j++)
+        {
+            sum = sum + matrix[j][i];
+        }
+        // if(maxsum < sum ){
+        //     maxsum = sum ;
+        // }
+        maxsum = max(sum, maxsum);
+    }
+    return maxsum;
+}
+
+//Digonal sum  (rows== column)
+//  \  -> primary digonal
+//   \ 
+
+//   /  -> secondary digonal
+//  /
+
+int main(){
+    int matrix[3][3] = { { 1,2,3}, {4,5,6} , {7,8,9}};
+    int rows =3;
+    int cols =3 ;
+   // cout<< "maximum row sum : "<< maxRowSum(matrix , rows , cols);
+    cout<< "maximum column sum : "<< maxColSum(matrix , rows , cols);
+}
