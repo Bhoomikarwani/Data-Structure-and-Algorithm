6169456a79c1e07ed3e448107b01d1d7114eff5e
Bhoomi Karwani <bhoomikarwani@gmail.com>
2026-04-04 23:40:39 +0530

    nth term of fibonacci with recursion

--- lect40/recursion.cpp
+++ lect40/recursion.cpp
@@ -14,13 +14,38 @@ using namespace std;
 // }
 
 // N fACTORIAL
-int factorial(int n){
-    if(n == 0){
-        return 1;
+// int factorial(int n){
+//     if(n == 0){
+//         return 1;
+//     }
+//     return n * factorial(n-1);
+// }
+// int main(){
+//    cout << factorial(5);
+//    return 0 ;
+// }
+
+// sum of n nums with recursion
+// int sum(int n){
+//    if(n == 1){
+//     return 1;
+//    }
+//    return n + sum(n-1);
+// }
+// int main(){
+//    int Sum =sum(4);
+//    cout << Sum ;
+// }
+
+//fibonacci (find (n) th trem of fibonacci series )
+int fib(int n){
+    if(n == 0 || n == 1){
+        return n;
     }
-    return n * factorial(n-1);
+    return fib(n-1) + fib(n-2);
 }
 int main(){
-   cout << factorial(5);
-   return 0 ;
+    int n = 6;
+    int val = fib(n);
+    cout << n <<"th term of fibonacci series is : "<< val;
 }
