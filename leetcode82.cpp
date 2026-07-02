0907d67ce6e9eecfae68db990c041715ad377d63
Bhoomi Karwani <bhoomikarwani@gmail.com>
2026-07-02 23:56:05 +0530

    remove duplicates from Linked List

---
+++ leetcode82.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <list>
+using namespace std;
+int main(){ 
+    
+    
+    //  if (head == NULL || head->next == NULL) return head;
+
+    //     ListNode* dummy = NULL // Dummy node to handle head removals
+    //     dummy->next = head;
+    //     ListNode* prev = dummy;
+    //     ListNode* cur = head;
+
+    //     while (cur != NULL && cur->next != NULL) {
+    //         if (cur->val == cur->next->val) {
+    //             // Skip all nodes with the same value
+    //             while (cur->next != NULL && cur->val == cur->next->val) {
+    //                 cur = cur->next;
+    //             }
+    //             prev->next = cur->next; // Remove duplicates
+    //         } else {
+    //             prev = prev->next; // Move to next distinct node
+    //         }
+    //         cur = cur->next;
+    //     }
+
+    //     return dummy->next;
+}
