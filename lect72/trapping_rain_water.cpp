56366849598884df7a266f562c650ad42178eabf
Bhoomi Karwani <bhoomikarwani@gmail.com>
2026-07-04 23:56:54 +0530

    Trapping rain water problem

---
+++ lect72/trapping_rain_water.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main(){
+    vector<int> height={4,2,0,3,2,5};
+    int n = height.size();
+    vector<int> lmax(n,0);
+    vector<int> rmax(n,0);
+
+    lmax[0] = height[0];
+    rmax[n-1] = height[n-1];
+    int ans = 0;
+
+    for(int i=1 ; i<n ; i++ ){
+        lmax[i] = max(lmax[i-1] , height[i]);
+    }
+    for(int i=n-2 ; i>=0 ; i-- ){
+        rmax[i] = max(rmax[i+2] , height[i]);
+    }
+    for(int i=0 ; i<n ; i++){
+        ans += min(lmax[i] , rmax[i]) - height[i];
+    }
+    cout << "total water : " << ans<<endl;
+}
