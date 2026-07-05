d3a38e895fb0c6c62cb7262022c3457e60f6117f
Bhoomi Karwani <bhoomikarwani@gmail.com>
2026-07-05 23:23:36 +0530

    Trapping rain water problem with optimal appraoch

--- lect72/trapping_rain_water.cpp
+++ lect72/trapping_rain_water.cpp
@@ -3,22 +3,45 @@
 using namespace std;
 int main(){
     vector<int> height={4,2,0,3,2,5};
+
+                                   // Better Approach O(n)
+    // int n = height.size();
+    // vector<int> lmax(n,0);
+    // vector<int> rmax(n,0);
+
+    // lmax[0] = height[0];
+    // rmax[n-1] = height[n-1];
+    // int ans = 0;
+
+    // for(int i=1 ; i<n ; i++ ){
+    //     lmax[i] = max(lmax[i-1] , height[i]);
+    // }
+    // for(int i=n-2 ; i>=0 ; i-- ){
+    //     rmax[i] = max(rmax[i+2] , height[i]);
+    // }
+    // for(int i=0 ; i<n ; i++){
+    //     ans += min(lmax[i] , rmax[i]) - height[i];
+    // }
+    // cout << "total water : " << ans<<endl;
+       
+                                 // Optimal Approach O(n)
     int n = height.size();
-    vector<int> lmax(n,0);
-    vector<int> rmax(n,0);
-
-    lmax[0] = height[0];
-    rmax[n-1] = height[n-1];
     int ans = 0;
+    int l=0 , r=n-1;
+    int lmax=0 , rmax =0 ;
+    while(l<r){
+        lmax = max(lmax , height[l]);
+        rmax = max(rmax , height[r]);
 
-    for(int i=1 ; i<n ; i++ ){
-        lmax[i] = max(lmax[i-1] , height[i]);
-    }
-    for(int i=n-2 ; i>=0 ; i-- ){
-        rmax[i] = max(rmax[i+2] , height[i]);
-    }
-    for(int i=0 ; i<n ; i++){
-        ans += min(lmax[i] , rmax[i]) - height[i];
-    }
-    cout << "total water : " << ans<<endl;
+        if(lmax < rmax){
+            ans += lmax-height[l];
+            l++;
+        }
+        else{
+            ans += rmax - height[r];
+            r--;
+        }
+
+    }    
+    cout << "total water : " << ans<<endl;        
 }
