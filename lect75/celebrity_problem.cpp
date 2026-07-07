64517fa603dea692d79c26774fc77ea6e9e443ba
Bhoomi Karwani <bhoomikarwani@gmail.com>
2026-07-08 00:04:42 +0530

    now it is an output

--- lect75/celebrity_problem.cpp
+++ lect75/celebrity_problem.cpp
@@ -6,6 +6,9 @@ using namespace std;
 int getCelebrity(vector<vector<int>>& arr){
     int n = arr.size();
     stack<int> s;
+    for(int i=0 ; i<n ;i++){
+      s.push(i);
+    }
 
     while(s.size()>1){
        int i = s.top();
@@ -28,12 +31,15 @@ int getCelebrity(vector<vector<int>>& arr){
             return -1;
           }
     }
+
     return celeb;
 }
 
 int main(){
      vector<vector<int>> arr = {{0,1,0} , {0,0,0} , {0,1,0}};
+ 
      int ans = getCelebrity(arr);
+
      cout << "celebrity is : " << ans << endl;
      return 0; 
 }
