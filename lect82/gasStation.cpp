641b0d112da9f95b501e76a06c873d2c7d97ae62
Bhoomi Karwani <bhoomikarwani@gmail.com>
2026-07-23 15:18:33 +0530

    gas station problem

---
+++ lect82/gasStation.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main(){
+    vector<int> gas = {5,1,2,3,4};
+    vector<int> cost = {4,4,1,5,1};
+
+    int totalGas = 0 , totalCost = 0;
+    for(int val : gas){
+        totalGas += val;
+    }
+    for(int val : cost){
+        totalCost += val;
+    }
+
+    if(totalGas < totalCost){
+        cout <<  -1;
+        return 0;
+    }
+    int currGas = 0 , start =0 ;
+    for(int i = 0 ; i < gas.size() ; i++){
+        currGas += gas[i] - cost[i];
+        if(currGas < 0 ){
+            start=i+1;
+            currGas = 0;
+        }
+    }
+    cout << start;
+    return  0; 
+}
