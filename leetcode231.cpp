cb61595b7c4fb7cc86e0368bba1219cafe5d46d4
Bhoomi Karwani <bhoomikarwani@gmail.com>
2026-07-29 23:18:22 +0530

    check whether the given no. is power of 2 or not

---
+++ leetcode231.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+ 
+bool isPowerOfTwo(int n) {
+       if (n <= 0 ){
+        return false;
+       }
+       while(n % 2 == 0){
+         n = n/2;
+       }
+       return n == 1; 
+}
+int main(){
+    int n = 8;
+    cout << isPowerOfTwo(n) << endl;
+}
