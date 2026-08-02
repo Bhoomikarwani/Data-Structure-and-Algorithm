ac6c5f601064dd3734889138f6bce7496d638f8a
Bhoomi Karwani <bhoomikarwani@gmail.com>
2026-08-03 00:01:54 +0530

    optimal approach to find power of 2

--- leetcode231.cpp
+++ leetcode231.cpp
@@ -2,13 +2,17 @@
 using namespace std;
  
 bool isPowerOfTwo(int n) {
-       if (n <= 0 ){
-        return false;
-       }
-       while(n % 2 == 0){
-         n = n/2;
-       }
-       return n == 1; 
+
+                                         // brute force approach
+      //  if (n <= 0 ){
+      //   return false;
+      //  }
+      //  while(n % 2 == 0){
+      //    n = n/2;
+      //  }
+      //  return n == 1; 
+                                         // optimal approach
+      return (n & (n-1)) == 0;                                   
 }
 int main(){
     int n = 8;
