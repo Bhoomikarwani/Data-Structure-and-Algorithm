9674091f5ac38d381d7a61d76f7396b420c830ff
Bhoomi Karwani <bhoomikarwani@gmail.com>
2026-08-04 23:55:59 +0530

    sum of data of nodes

--- lect83/binaryTree.cpp
+++ lect83/binaryTree.cpp
@@ -109,6 +109,17 @@ int count(Node* root){
     return leftCount + rightCount + 1;
 }
 
+// sum of nodes
+int sum(Node* root){
+  if(root == NULL){
+    return 0;
+  }
+  int leftSum = sum(root->left);
+  int rightSum = sum(root->right);
+
+  return leftSum + rightSum + root->data;
+}
+
 int main(){
     vector<int> preorder = {1,2,-1,-1,3,4,-1,-1,5,-1,-1};
 
@@ -125,6 +136,7 @@ int main(){
     // cout << endl ; 
     //levelOrder(root);
     //cout  << height(root);
-    cout << count(root);
+    //cout << count(root);
+    cout << sum(root) ; 
     return 0;
 }
