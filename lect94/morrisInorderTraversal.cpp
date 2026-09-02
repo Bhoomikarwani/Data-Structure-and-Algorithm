4e48b7bcba459d19583bd62700bd8efd839a7b50
Bhoomi Karwani <bhoomikarwani@gmail.com>
2026-09-02 11:08:00 +0530

    morris algorithm

--- lect94/morrisInorderTraversal.cpp
+++ lect94/morrisInorderTraversal.cpp
@@ -30,8 +30,6 @@ Node* buildTree(vector<int> preorder){
     return root;
 } 
 void helper(Node* root , vector<int>& ans){
-
-                              // itreative approach
         if(root == NULL){                
             return;                      
         } 
@@ -39,25 +37,38 @@ void helper(Node* root , vector<int>& ans){
         while(curr != NULL){
             if(curr->left == NULL){
                 ans.push_back(curr->data);
-                if(curr->right != NULL){
-                   curr = curr->right;
-                }
-                else{
-                    
-                }
+                curr = curr->right;
             }
             else{
-                curr = curr->left;
+                // find Inorder Predecessors
+                Node* IP = curr->left;
+                while(IP->right != NULL && IP->right != curr){
+                    IP = IP->right;
+                }
+                if(IP->right == NULL){
+                    IP->right = curr;     // create thread
+                    curr = curr->left;
+                }
+                else{   // IP->right != NULL
+                    IP->right = NULL;            // delete thread
+                    ans.push_back(curr->data);
+                    curr = curr->right; 
+                }
             }
-        }                      
+        }        
+        return ;              
 
     }
 vector<int> inorderTraversal(Node* root){
-        vector<int> ans;
-        helper(root , ans);
-        return ans;
-}    
+    vector<int> ans;
+    helper(root , ans);
+    return ans;
+}
 int main(){
-     vector<int> preorder = {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
+    vector<int> preorder = {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
     Node* root = buildTree(preorder);
+    vector<int> ans = inorderTraversal(root);
+    for(int i : ans){
+        cout << i << endl;
+    }  
 }
