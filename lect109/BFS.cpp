9f0fa3d94deb4f06f72b67f0084fce40478f2b85
Bhoomi Karwani <bhoomikarwani@gmail.com>
2026-09-20 23:55:26 +0530

    bfs

---
+++ lect109/BFS.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+#include <list>
+#include <queue>
+using namespace std;
+                                                         //Building Graph
+class Graph{
+    int V;
+    list<int> *l ;  // int *arr (dynamnic array)
+
+public:
+    Graph(int V){
+       this->V = V;
+       l = new list<int>[V]; // arr = new int[v]  =>initializing and allocating size;  
+    } 
+
+    void addEdge(int u , int v){
+      l[u].push_back(v);
+      l[v].push_back(u);
+    }
+
+    void bfs(){
+         queue<int> q;
+         vector<bool> visited(V, false);
+
+         q.push(0);
+         visited[0] = true;
+
+         while(q.size() > 0){
+            int u = q
+         }
+    }
+};
+
+int main(){
+    Graph g(5);
+
+    g.addEdge(0,1);
+    g.addEdge(1,2);
+    g.addEdge(1,3);
+    g.addEdge(2,3);
+    g.addEdge(2,4);
+
+    g.bfs();
+}
