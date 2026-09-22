0c575dd00b64b75ca1ad30f54c67209a46fd37c6
Bhoomi Karwani <bhoomikarwani@gmail.com>
2026-09-22 23:59:26 +0530

    problem 653

--- leetcode.cpp
+++ leetcode.cpp
@@ -1,27 +1,46 @@
 #include <iostream>
 #include <vector>
-#include <unordered_map>
+#include <unordered_set>
 using namespace std;
-int countSpecialIntegers(vector<int>& nums) {
-        unordered_map<int , vector<int>> indices;
-        int count = 0 ;
-        for(int i=0 ; i<nums.size() ; i++){
-            indices[nums[i]].push_back(i);
-        }
-        for(const auto& val : indices)
-        {
-            if(val.second.size() == 3)
-            {
-                if(val.second[1] - val.second[0]  ==  val.second[2] - val.second[1])
-                {
-                    count++;
-                }
-            }
-        }
-        
-        return count;
+// Given the root of a binary search tree and an integer k, return true if there exist two elements in the BST such that their sum is equal to k, or false otherwise
+class Node{
+  public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val){
+        data = val;
+        left = right = NULL;
+    }
+};
+
+
+unordered_set<int> s;
+bool findTarget(Node* root, int k) {
+      
+       if(root == NULL ) return false;
+
+       if(s.count(k-root->data)){   //count() function ka use yeh check karne ke liye hota 
+          return true;              // hai ki koi element set me present hai ya nahi
+       } 
+       else{
+          s.insert(root->data);
+       }
+
+       return findTarget(root->left , k ) || findTarget(root->right , k);
+       
+       
 }
+
 int main(){
-    vector<int> nums ={1,2,3,41,2,3,41,3};
-    cout << countSpecialIntegers(nums);
-}
+    Node* root = new Node(6);
+    root->left = new Node(4);
+    root->right = new Node(8);
+    root->left->left = new Node(1);
+    root->left->right = new Node(5);
+    root->right->left = new Node(7);
+    root->right->right = new Node(9);
+
+    cout << findTarget(root , 9);
+}    
